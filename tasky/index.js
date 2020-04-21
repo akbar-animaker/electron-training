d470845b499933f1e5fcf3d5b7a7117fab80e032
Akbar Basha KM <akbar@animaker.com>
2020-04-21 19:55:32 +0530

    icons added in electron application

--- tasky/index.js
+++ tasky/index.js
@@ -1,7 +1,9 @@
 const eletron = require('electron');
-const { app, BrowserWindow } = eletron;
+const path = require('path');
+const { app, BrowserWindow, Tray } = eletron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
@@ -11,7 +13,32 @@ app.on('ready', () => {
         width: 300,
         height: 500,
         frame: false,
-        resizable: false
+        resizable: false,
+        show: false
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+    tray = new Tray(iconPath);
+
+    tray.on('click', (evt, bounds) => {
+        const { x, y } = bounds;
+        const { width, height } = mainWindow.getBounds();
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+        } else {
+            /**
+             * This is because OSX icons displayed in top but window, icons displayed in bottom
+             */
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            mainWindow.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                width,
+                height
+            })
+            mainWindow.show();
+        }
+    })
 })
